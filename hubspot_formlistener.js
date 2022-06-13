26591c61f244a7c91eafcb69214732a6d3a75726
SMMaEi <107404991+SMMaEi@users.noreply.github.com>
2022-06-13 13:03:07 +0200

    Create hubspot_formlistener.js

---
+++ hubspot_formlistener.js
@@ -0,0 +1,8 @@
+window.addEventListener("message", function(event) {
+  if(event.data.type === 'hsFormCallback'  &&  event.data.eventName === 'onFormSubmitted') {
+    window.dataLayer.push({
+      'event': 'hubspot-form-success',
+      'hs-form-guid': event.data.id
+    });
+  }
+});
